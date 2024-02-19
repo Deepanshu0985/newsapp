9c48befb20b299f5639fac7a8b5db79b01ac5d42
Deepanshu0985 <deepanshuy098@gmail.com>
2024-02-19 14:58:46 +0530

    Api fetch complete

--- src/components/Newsitem.jsx
+++ src/components/Newsitem.jsx
@@ -5,11 +5,11 @@ export default class Newsitem extends Component {
     let {title, description,imageurl,url} = this.props;
     return (
         <div className="card" style={{width: "18rem"}}>
-            <img src={imageurl} className="card-img-top" alt="..."/>
+            <img src={imageurl} className="card-img-top" alt="..." />
             <div className="card-body">
                 <h5 className="card-title">{title +"..."}</h5>
                 <p className="card-text">{description +"..."}</p>
-                <a href={url} target='_blank' className="btn btn-sm btn-primary">Read more</a>
+                <a href={url} rel="noreferrer" target='_blank' className="btn btn-sm btn-primary">Read more</a>
             </div>
         </div>
           
--- src/components/Newspage.jsx
+++ src/components/Newspage.jsx
@@ -11,40 +11,58 @@ export default class Newspage extends Component {
         this.state = {
             articles : [],
             loading : false,
-            page : 1
+            page : 1,
+            result : 0
+           
                        
         }
         
     }
 
     async componentDidMount(){
-        let url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=bdcbde0a23a340aa83e3ba4f7c092422`;
+        let url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=bdcbde0a23a340aa83e3ba4f7c092422&pageSize=12`;
         let data =  await fetch(url);
         let parseddata = await data.json(); 
-        console.log(parseddata);
+        console.log(parseddata.totalResults);
         this.setState({articles: parseddata.articles,
-            page:this.state.page 
+            page:this.state.page ,
+            result : parseddata.totalResults
+            
         });
+       
+        
     }
 
     handlenextclick =async()=>{
-        let url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=bdcbde0a23a340aa83e3ba4f7c092422&page=${this.state.page+1}`;
+        let url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=bdcbde0a23a340aa83e3ba4f7c092422&page=${this.state.page+1}&pageSize=12`;
         let data =  await fetch(url);
         let parseddata = await data.json(); 
         console.log(parseddata);
         this.setState({
             page:this.state.page+1,
             articles: parseddata.articles,
+            result : parseddata.totalResults
              
-        });
+        });       
+    }
 
-        
+    handleprevclick =async()=>{
+        let url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=bdcbde0a23a340aa83e3ba4f7c092422&page=${this.state.page-1}&pageSize=12`;
+        let data =  await fetch(url);
+        let parseddata = await data.json(); 
+        console.log(parseddata);
+        this.setState({
+            page:this.state.page-1,
+            articles: parseddata.articles,
+            result : parseddata.totalResults
+        })
+
     }
   render() {
     
     return (
      
-        <div className='container my-4'>
+        <div className='container my-2'>
             <h2>Top - Headlines</h2>
             <div className="row">
                 {
@@ -60,8 +78,8 @@ export default class Newspage extends Component {
             </div>
 
             <div class="d-flex justify-content-between">
-                <button type="button" class="btn btn-dark" onClick={this.handleprevclick}>&larr; Previous </button>
-                <button type="button" class="btn btn-dark" onClick={this.handlenextclick}>Next &rarr;</button>
+                <button disabled={this.state.page===1} type="button" class="btn btn-dark" onClick={this.handleprevclick}>&larr; Previous </button>
+                <button disabled={this.state.page===Math.ceil(this.state.result/12)} type="button" class="btn btn-dark" onClick={this.handlenextclick}>Next &rarr;</button>
             </div>
 
         </div>
